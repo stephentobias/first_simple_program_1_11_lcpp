27c257eb5ef6c6a3309cfd21f89e7c2771e177a2
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-02-27 17:01:11 -0600

    Minor corrections to code per lesson standards
    Make following corrections:
      - Correctly initialize variable; change from default-initilization to value-initialization.
      - Change 'newline' after input from 'std::endl' to '\n'

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ int main()
 {
 	std::cout << "Enter an integer: ";
 	
-	int user_input;
+	int user_input{ };
 	std::cin >> user_input;
 
-	std::cout << "Double that number is: " << 2 * user_input << std::endl;
+	std::cout << "Double that number is: " << 2 * user_input << '\n';
 	return 0;
 }
