3efdbcd180299fb8f71d96bdc891d891d8a3f509
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-02-27 16:45:59 -0600

    Create a program that doubles an integer
    The user is prompted for an integer then the program returns double that input.
    
    This program is an attempt to write my own solution to the lesson prompt before seeing the solution.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter an integer: ";
+	
+	int user_input;
+	std::cin >> user_input;
+
+	std::cout << "Double that number is: " << 2 * user_input << std::endl;
+	return 0;
+}
