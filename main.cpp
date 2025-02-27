426ba8e59705c6f0e36fb9ed80fbe0bcb4adf61c
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-02-27 17:08:32 -0600

    Modify to show double and triple user input
    Answer to question 1 in lesson where both double and triple the user input is output on the console.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main()
 	int user_input{ };
 	std::cin >> user_input;
 
-	std::cout << "Double that number is: " << 2 * user_input << '\n';
+	std::cout << "Double " << user_input << " is: " << 2 * user_input << '\n';
+	std::cout << "Triple " << user_input << " is: " << 3 * user_input << '\n';
+
 	return 0;
 }
