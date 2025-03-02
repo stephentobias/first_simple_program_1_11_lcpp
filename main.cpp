17864b91f2609a6915019dcbc61dcfb81ec13e1f
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 21:09:01 -0600

    Split user input functionality into a functionality
    Creating a new function to grab user input to practice writing functions per chapter 2 lesson 2.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 
+int getNumber();
+
 int main()
 {
-	std::cout << "Enter an integer: ";
-	
 	int user_input{ };
-	std::cin >> user_input;
+	user_input = getNumber();
 
 	std::cout << "Double " << user_input << " is: " << 2 * user_input << '\n';
 	std::cout << "Triple " << user_input << " is: " << 3 * user_input << '\n';
 
 	return 0;
+}
+
+int getNumber()
+{
+	std::cout << "Enter an integer: ";
+	int user_input{ };
+	std::cin >> user_input;
+
+	return user_input;
 }
