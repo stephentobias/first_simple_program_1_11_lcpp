7d7f3c91e24899fc08f30c395896fc99795c4dab
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 22:18:30 -0600

    Create another example using user-defined function
    Additional practice using functions.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@ int getNumberFromUser();
 
 int main()
 {
-	int user_input{ getNumberFromUser() };
+	int user_input1{ getNumberFromUser() };
+	int user_input2{ getNumberFromUser() };
 
-	std::cout << "Double " << user_input << " is: " << 2 * user_input << '\n';
-	std::cout << "Triple " << user_input << " is: " << 3 * user_input << '\n';
+	std::cout << user_input1 << " + " << user_input2 << " = " << user_input1 + user_input2 << '\n';
 
 	return 0;
 }
 
+/*
+* Function prompts user for an integer input and returns it to the caller
+*/
 int getNumberFromUser()
 {
 	std::cout << "Enter an integer: ";
