fe3da661013a4e8187f309b5d088756b7798aeb2
stephentobias <200682130+stephentobias@users.noreply.github.com>
2025-03-01 21:20:19 -0600

    Change code to match example in lesson
    Make the following changes:
    	- Rename the user-defined function to be more descriptive.
    	- Initialize the 'user_input' variable in main() and call getNumberFromUser() function in a single statement instead of two.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-int getNumber();
+int getNumberFromUser();
 
 int main()
 {
-	int user_input{ };
-	user_input = getNumber();
+	int user_input{ getNumberFromUser() };
 
 	std::cout << "Double " << user_input << " is: " << 2 * user_input << '\n';
 	std::cout << "Triple " << user_input << " is: " << 3 * user_input << '\n';
@@ -13,7 +12,7 @@ int main()
 	return 0;
 }
 
-int getNumber()
+int getNumberFromUser()
 {
 	std::cout << "Enter an integer: ";
 	int user_input{ };
